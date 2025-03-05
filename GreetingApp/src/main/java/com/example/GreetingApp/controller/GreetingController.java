8bbe2497e61ba0f19780247a0eed1fd8d95c4ffe
Aditya <adityasinghbtp123@gmail.com>
2025-03-05 15:38:44 +0530

    first commit

--- GreetingApp/src/main/java/com/example/GreetingApp/controller/GreetingController.java
+++ GreetingApp/src/main/java/com/example/GreetingApp/controller/GreetingController.java
@@ -1,25 +1,45 @@
 package com.example.GreetingApp.controller;
 
 import com.example.GreetingApp.model.Greeting;
+import com.example.GreetingApp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+
+
 @RestController
-@RequestMapping("/greetings")
 public class GreetingController {
-    @GetMapping
+
+    @RequestMapping("/")
+    public String sayHello(){
+        return "Hello from bridgeLab";
+    }
+
+    @GetMapping("/greetings")
     public Greeting getGreetings(){
         return new Greeting("Hello , My name is Aditya singh !");
     }
-    @PostMapping
+
+    @PostMapping("/greetings")
     public Greeting createGreeting(@RequestBody Greeting newGreeting){
         return new Greeting("Greeting Created :"+newGreeting.getMessage());
     }
-    @PutMapping
+
+    @PutMapping("/greetings")
     public Greeting updateGreeting(@RequestBody Greeting updatedGreeting){
         return new Greeting("Greetings Updated :"+updatedGreeting.getMessage());
     }
-    @DeleteMapping
+
+    @DeleteMapping("/greetings")
     public Greeting deleteGreeting(){
         return new Greeting("Greeting Deleted ");
     }
+
+//    UC2
+    @Autowired
+    GreetingService greetingService;
+
+
+
+
 }
